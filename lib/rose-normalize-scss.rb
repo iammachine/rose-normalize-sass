e7aacd3e9ad1131e0f0d1290bf110b4b0eb7f90f
iammachine <iammachine@users.noreply.github.com>
2015-07-11 16:36:30 +0530

    update of rose-normalize-scss.rb - commit.
    @require - compass, http://compass-style.org
    @require - Ruby, https://www.ruby-lang.org/

--- lib/rose-normalize-scss.rb
+++ lib/rose-normalize-scss.rb
@@ -1,1 +1,6 @@
 require 'compass'
+
+
+project_path     = File.join(File.dirname(__FILE__), '..')
+stylesheets_path = File.join(project_path)
+templates_path   = File.join(project_path, 'templates')
