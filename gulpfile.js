0ffcf3912e9d1a878a2de6ded6a41a0bd03d73f1
iammachine <iammachine@users.noreply.github.com>
2015-07-11 17:54:01 +0530

    initial gulpfile.js - commit.

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/*!
+ * rose-normalize-sass [v0.0.1]
+ * ___________________________________________________
+ * Gulp: The streaming build system.
+ * https://github.com/gulpjs/gulp | http://gulpjs.com/
+ * @author  : Prabhat Kumar, http://prabhatkumar.org/
+ * @date    : 11-July-2015
+ * ___________________________________________________
+ */
