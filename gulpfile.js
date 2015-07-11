94fbde75146e0952e6b6bd2807486fba2ce0dbf6
iammachine <iammachine@users.noreply.github.com>
2015-07-11 17:58:28 +0530

    update of gulpfile.js - commit.
    Building With Gulp, http://gulpjs.com/ and https://github.com/gulpjs/gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -30,3 +30,16 @@ var rename          = require("gulp-rename");
 var replace        = require('gulp-replace');
 var bytediff      = require('gulp-bytediff');
 var chmod            = require('gulp-chmod');
+
+// ***** SETTINGS ***** //
+var AUTOPREFIXER_BROWSERS = [
+  'ie >= 10',
+  'ie_mob >= 10',
+  'ff >= 30',
+  'chrome >= 34',
+  'safari >= 7',
+  'opera >= 22',
+  'ios >= 7',
+  'android >= 4.4',
+  'bb >= 10'
+];
